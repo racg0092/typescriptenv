356f92e13130b141296f899fac44dd57790d5847
racg0092 <chapman.gomez.richard@gmail.com>
2020-02-06 09:12:21 -0500

    babelify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var tsify = require('tsify');
-var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
 var paths = {
@@ -23,11 +22,14 @@ gulp.task('default', gulp.series(gulp.parallel('copy-html'), function () {
         packageCache: {}
     })
     .plugin(tsify)
+    .transform('babelify', {
+        presets: ['es2015'],
+        extensions: ['.ts']
+    })
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 }));
